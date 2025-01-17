a03eb019c26fb40e7eee7d348198e5e88a13154f
caiopgaspar <caio_gaspar@yahoo.com.br>
2025-01-17 16:35:48 -0300

    Criação ta tela de login, atalho para opções de usuário

--- RedeSocial/Program.cs
+++ RedeSocial/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://sua-api-endpoint.com/") });
 
 builder.Services.AddScoped<PublicacaoService>();
@@ -13,3 +14,5 @@ builder.Services.AddScoped<PublicacaoService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+
